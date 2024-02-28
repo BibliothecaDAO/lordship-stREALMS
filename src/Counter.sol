23b7f6de7eb56a6ffd32ad3f5b89c9ef30b8a90e
ponderingdemocritus <ponderingdemocritus@protonmail.com>
2024-02-28 16:37:01 +1100

    init

--- src/Counter.sol
+++ src/Counter.sol
@@ -1,14 +1,29 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-contract Counter {
-    uint256 public number;
 
-    function setNumber(uint256 newNumber) public {
-        number = newNumber;
+import "lib/openzeppelin-contracts/contracts/token/ERC721/ERC721.sol";
+import "lib/openzeppelin-contracts/contracts/utils/cryptography/EIP712.sol";
+import "lib/openzeppelin-contracts/contracts/token/ERC721/extensions/ERC721Votes.sol";
+import "lib/openzeppelin-contracts/contracts/token/ERC721/extensions/ERC721Wrapper.sol";
+
+contract RealmsLordShip is ERC721, EIP712, ERC721Votes, ERC721Wrapper {
+    constructor() ERC721("MyToken", "MTK") EIP712("MyToken", "1") ERC721Wrapper("0x0") {}
+
+    // The following functions are overrides required by Solidity.
+
+    function _update(address to, uint256 tokenId, address auth)
+        internal
+        override(ERC721, ERC721Votes)
+        returns (address)
+    {
+        return super._update(to, tokenId, auth);
     }
 
-    function increment() public {
-        number++;
+    function _increaseBalance(address account, uint128 value)
+        internal
+        override(ERC721, ERC721Votes)
+    {
+        super._increaseBalance(account, value);
     }
 }
