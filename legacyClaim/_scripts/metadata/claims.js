319a244983437b96aafac73096f65253648c6277
Credence <hello@zerocredence.com>
2024-08-26 11:20:45 +0100

    update metadata generation script

--- legacyClaim/_scripts/metadata/claims.js
+++ legacyClaim/_scripts/metadata/claims.js
@@ -1,8 +1,50 @@
 const fs = require("fs");
+const csv = require("csv-parse/sync");
 const path = require("path");
-const assert = require("assert").strict;
+const claimsVSCFilePath = path.join(__dirname, "claims.csv");
+const claimsJSONFilePath = path.join(__dirname, "claims.json");
+
+/////////////////////////////////////////////////
+//// convert claims.csv to json
+/////////////////////////////////////////////////
+
+// Function to convert decimal to hexadecimal
+function decimalToHex(decimal) {
+  return "0x" + decimal.toString(16).toUpperCase();
+}
+
+// Read the CSV file
+const claimsCSVFileContent = fs.readFileSync(claimsVSCFilePath, "utf-8");
+// Parse the CSV content
+const claimsRecords = csv.parse(claimsCSVFileContent, {
+  delimiter: ",",
+  skip_empty_lines: true,
+});
+
+
+// Process the claimsRecords and create the output object
+const claimsJson = {};
+claimsRecords.forEach((record, index) => {
+  const [address, amount] = record;
+  claimsJson[index] = {
+    address: address,
+    amount: decimalToHex(BigInt(amount) * BigInt(10 ** 18)),
+  };
+});
+
+// Convert the output object to a JSON string
+const claimsJsonOutput = JSON.stringify(claimsJson, null, 2);
+// Write the JSON to a file
+fs.writeFileSync(claimsJSONFilePath, claimsJsonOutput);
+
+console.log("\n\n Conversion complete from claims.csv to claims.json \n\n");
+
+
+/////////////////////////////////////////////////
+//// convert claims.json to claims.cairo
+/////////////////////////////////////////////////
+
 // Define paths to the input and output files
-const inputFilePath = path.join(__dirname, "claims.json");
 const cairoFileName = "claims.cairo";
 const cairoFilePath = path.join(__dirname, "..","..", "starknet", "src", cairoFileName);
 
@@ -18,45 +60,15 @@ const generateMappingFunction = (mapping, name) => {
 };
 
 
-// Read the custom.json file
-fs.readFile(inputFilePath, "utf8", (err, data) => {
+// Read the claims.json file
+let generatedCairoFile = generateMappingFunction(claimsJson, "claims");
+fs.writeFile(cairoFilePath, generatedCairoFile, (err) => {
   if (err) {
-    console.error("Error reading the file:", err);
-    return;
+    console.error("Error writing file:", err);
+  } else {
+    console.log(`File saved successfully as ${cairoFileName}`);
   }
-
-  // Parse the JSON data
-  let jsonData;
-  try {
-    jsonData = JSON.parse(data);
-  } catch (parseErr) {
-    console.error("Error parsing JSON data:", parseErr);
-    return;
-  }
-
-    let generatedCairoFile = generateMappingFunction(jsonData, "claims");
-    fs.writeFile(cairoFilePath, generatedCairoFile, (err) => {
-      if (err) {
-        console.error("Error writing file:", err);
-      } else {
-        console.log(`File saved successfully as ${cairoFileName}`);
-      }
-    });
 });
 
 
-
-
-  // // generate cairo file to store compressed json 
-  // fs.writeFile(
-  //   cairoFilePath,
-  //   generateCompressDataFunction(jsonData),
-  //   "utf8",
-  //   (err) => {
-  //     if (err) {
-  //       console.error("Error writing the file:", err);
-  //     } else {
-  //       console.log(`Updated data has been saved to ${cairoFilePath}`);
-  //     }
-  //   }
-  // );
+console.log("\n\n Conversion complete from claims.json to claims.cairo \n\n");
